c56c529813256e63f638f20cbe8c0bd1e79e9f41
Zach Martin <zmironman@gmail.com>
2020-10-13 09:29:40 -0500

    Lint fixes

--- src/components/ResultView.js
+++ src/components/ResultView.js
@@ -1,14 +1,6 @@
-import {Button, Card} from 'react-bootstrap';
+import {Card} from 'react-bootstrap';
 import React from 'react';
-import ReactDOM from 'react-dom'
-
-const ipRegex = RegExp(/^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/);
-
 export default class ResultView extends React.Component {
-    constructor(props) {
-        super(props);
-    }
-
     render() {
         return (
             <Card className='mb-lg-3 mt-lg-3'>
